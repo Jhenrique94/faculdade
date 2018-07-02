a17598060c0a29bb13425f7f4dc5e9294ee0396f
Jonathan Henrique <jhodesenv@gmail.com>
2018-07-02 19:58:20 -0300

    relatorio corrigido

--- lm/trabalho/tamanho.h
+++ lm/trabalho/tamanho.h
@@ -1,4 +1,4 @@
-#define A_Linhas  1000
-#define A_Colunas 1000
-#define C_Linhas  1000
-#define C_Colunas 1000
+#define A_Linhas  100
+#define A_Colunas 100
+#define C_Linhas  100
+#define C_Colunas 100
